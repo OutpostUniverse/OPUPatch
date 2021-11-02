9f9ab1224e3ff2a040f54c4af02f7cd821f990c6
Arklon1 <16932296+Arklon1@users.noreply.github.com>
2021-11-02 00:29:46 -0400

    Add OnTransferUnit() and OnProcessCommand() mission user callbacks

--- Mission.cpp
+++ Mission.cpp
@@ -9,7 +9,9 @@
 #include "Tethys/API/TethysGame.h"
 #include "Tethys/API/Trigger.h"
 #include "Tethys/Game/MissionManager.h"
+#include "Tethys/Game/CommandPacket.h"
 #include "Tethys/Game/GameStartInfo.h"
+#include "Tethys/Game/PlayerImpl.h"
 #include "Tethys/Game/ScBase.h"
 #include "Tethys/Resource/StreamIO.h"
 
@@ -41,7 +43,7 @@ static ScStubListEx g_scStubListEx;
 
 // =====================================================================================================================
 // Adds new callback entry points for mission DLLs.
-// ** TODO fix OnDestroyUnit to handle weapons fire/Pretty art (moDelSelf action)
+// ** TODO Add OnTransferUnit and OnProcessCommandPacket
 bool SetMissionCallbackPatch(
   bool enable)
 {
@@ -59,19 +61,23 @@ bool SetMissionCallbackPatch(
       void  (__cdecl*  pfnOnCreateUnit)(OnCreateUnitArgs&&);
       void  (__cdecl*  pfnOnDestroyUnit)(OnDestroyUnitArgs&&);
       void  (__cdecl*  pfnOnDamageUnit)(OnDamageUnitArgs&&);
+      void  (__cdecl*  pfnOnTransferUnit)(OnTransferUnitArgs&&);
+      void  (__cdecl*  pfnOnProcessCommand)(OnProcessCommandArgs&&);
     } callbacks = { };
 
     // Insert OnLoadMission() hook and initialize callbacks In MissionManager::LoadScript()
     patcher.LowLevelHook(0x402C0B, [](Esi<MissionManager*> pThis) {
-      (FARPROC&)(callbacks.pfnOnLoad)        = GetProcAddress(pThis->hModule_, "OnLoadMission");
-      (FARPROC&)(callbacks.pfnOnUnload)      = GetProcAddress(pThis->hModule_, "OnUnloadMission");
-      (FARPROC&)(callbacks.pfnOnEnd)         = GetProcAddress(pThis->hModule_, "OnEndMission");
-      (FARPROC&)(callbacks.pfnOnSave)        = GetProcAddress(pThis->hModule_, "OnSaveGame");
-      (FARPROC&)(callbacks.pfnOnLoadSave)    = GetProcAddress(pThis->hModule_, "OnLoadSavedGame");
-      (FARPROC&)(callbacks.pfnOnChat)        = GetProcAddress(pThis->hModule_, "OnChat");
-      (FARPROC&)(callbacks.pfnOnCreateUnit)  = GetProcAddress(pThis->hModule_, "OnCreateUnit");
-      (FARPROC&)(callbacks.pfnOnDestroyUnit) = GetProcAddress(pThis->hModule_, "OnDestroyUnit");
-      (FARPROC&)(callbacks.pfnOnDamageUnit)  = GetProcAddress(pThis->hModule_, "OnDamageUnit");
+      (FARPROC&)(callbacks.pfnOnLoad)           = GetProcAddress(pThis->hModule_, "OnLoadMission");
+      (FARPROC&)(callbacks.pfnOnUnload)         = GetProcAddress(pThis->hModule_, "OnUnloadMission");
+      (FARPROC&)(callbacks.pfnOnEnd)            = GetProcAddress(pThis->hModule_, "OnEndMission");
+      (FARPROC&)(callbacks.pfnOnSave)           = GetProcAddress(pThis->hModule_, "OnSaveGame");
+      (FARPROC&)(callbacks.pfnOnLoadSave)       = GetProcAddress(pThis->hModule_, "OnLoadSavedGame");
+      (FARPROC&)(callbacks.pfnOnChat)           = GetProcAddress(pThis->hModule_, "OnChat");
+      (FARPROC&)(callbacks.pfnOnCreateUnit)     = GetProcAddress(pThis->hModule_, "OnCreateUnit");
+      (FARPROC&)(callbacks.pfnOnDestroyUnit)    = GetProcAddress(pThis->hModule_, "OnDestroyUnit");
+      (FARPROC&)(callbacks.pfnOnDamageUnit)     = GetProcAddress(pThis->hModule_, "OnDamageUnit");
+      (FARPROC&)(callbacks.pfnOnTransferUnit)   = GetProcAddress(pThis->hModule_, "OnTransferUnit");
+      (FARPROC&)(callbacks.pfnOnProcessCommand) = GetProcAddress(pThis->hModule_, "OnProcessCommand");
 
       return ((callbacks.pfnOnLoad == nullptr) || callbacks.pfnOnLoad({ sizeof(OnLoadMissionArgs) })) ? 0 : 0x402B65;
     });
@@ -166,6 +172,22 @@ bool SetMissionCallbackPatch(
     // In Vortex::DoEvent()
     patcher.LowLevelHook(0x48F9C7, [](Esp<void*> pEsp, Ebx<MapObject*> pTarget, Esi<int16> damage)
       { OnDamageUnit(PtrInc<MapObj::Vortex*>(pEsp, 16), pTarget, damage); });
+
+    // Insert OnTransferUnit() hook in MissionManager::OnTransferUnit()
+    patcher.LowLevelHook(0x4031F2, [](Esi<MapObject*> pMo, Esp<void*> pEsp) {
+      if (callbacks.pfnOnTransferUnit != nullptr) {
+        callbacks.pfnOnTransferUnit(
+          { sizeof(OnTransferUnitArgs), Unit(pMo), *PtrInc<int*>(pEsp, 24), *PtrInc<int*>(pEsp, 28) });
+      }
+    });
+
+    // Insert OnProcessCommand() hook in PlayerImpl::ProcessCommandPacket()
+    patcher.LowLevelHook(0x40E313, [](Ecx<PlayerImpl*> pThis, Edi<CommandPacket*> pPacket) {
+      const CommandType type = pPacket->type;
+      if ((callbacks.pfnOnProcessCommand) && (type != CommandType::Nop) && (type != CommandType::InvalidCommand)) {
+        callbacks.pfnOnProcessCommand({ sizeof(OnProcessCommandArgs), pPacket, pThis->playerNum_});
+      }
+    });
 
     // Pass extended params to triggers, i.e. function signature is now of the form void __cdecl(OnTriggerArgs*).
     // Because trigger callbacks always used cdecl (caller cleanup), this is ABI-compatible with existing callbacks.
